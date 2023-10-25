d6b7e6e8d566f6e30063fb60339cb1418e92cdb0
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-25 11:01:42 +0530

    spring-boot-3

---
+++ src/main/java/com/excellence_spring_boot_unit_test_rest_controller/SpringBootUnitTestRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_unit_test_rest_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestRestControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_unit_test_rest_controller/SpringBootUnitTestRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_unit_test_rest_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
